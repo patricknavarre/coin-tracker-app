5678868cfcbc8a435c9be47c77f265d9feef2998
Patrick <patnavarre@gmail.com>
2021-04-29 16:20:27 -0400

    authCoinHome under construction

--- src/components/AuthCoinHome/AuthCoinHome.js
+++ src/components/AuthCoinHome/AuthCoinHome.js
@@ -1,10 +1,72 @@
-import React from 'react'
+import React, { useContext, useState, useEffect } from 'react'
+import { Link } from "react-router-dom";
+import axios from 'axios';
+
+import { makeStyles } from "@material-ui/core/styles";
+
+import {
+    FormControl,
+    FormHelperText,
+    Input,
+    InputLabel,
+    Button,
+    CircularProgress,
+    Snackbar,
+    Grid,
+  } from "@material-ui/core";
+  
+  import MuiAlert from "@material-ui//lab/Alert";
+import { AuthContext } from '../context/AuthContext';
+
+  const useStyles = makeStyles((theme) => ({
+    root: {
+      "& > *": {
+        margin: theme.spacing(1),
+        width: 350,
+      },
+    },
+  }));
+
 
 function AuthCoinHome() {
+
+    const classes = useStyles();
+
+    const { dispatch } = useContext(AuthContext);
+
+    const [search, setSearch] = useState("")
+
+    useEffect(() => {
+        
+        
+    })
+
+
+
+
     return (
-        <div>
-            AuthCoinHome
-        </div>
+        <Grid container spacing={0}
+        direction="column"
+        alignItems="center"
+        justify="center"
+        style={{ minHeight: "30vh" }}
+        >
+        <InputLabel htmlFor="component-search">Search</InputLabel>
+            <Input
+              id="component-search"
+              name="search"
+              value={search}
+              onChange={(e) => setSearch(e)}
+            />
+       <Button
+            variant="contained"
+            color="primary"
+            type="submit"
+          >
+            Submit
+          </Button>
+      
+      </Grid>
     )
 }
 
