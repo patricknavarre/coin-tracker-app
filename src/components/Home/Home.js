e627aee94ff169dbebb54f285849a88055b7430c
Patrick <patnavarre@gmail.com>
2021-06-16 13:54:03 -0400

    centered homepage text

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Home() {
     return (
-        <div>
+        <div style={{ textAlign: "center" }}>
             Please Go Sign Up to get the latest Crypto Prices!
         </div>
     )
