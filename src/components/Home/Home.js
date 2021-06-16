533d67af020f59541acfa27097567dff878040a5
Patrick <patnavarre@gmail.com>
2021-06-16 15:21:14 -0400

    added images

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,17 @@
 import React from 'react'
+import CryptoPics from '../../images/cryptoPics.jpeg'
+
+
 
 function Home() {
     return (
         <div style={{ textAlign: "center" , marginTop: "10px" }}>
             Please Go Sign Up To Get The Latest Crypto Prices!
+        <div>
+        <CryptoPics />
         </div>
+        </div>
+        
     )
 }
 
